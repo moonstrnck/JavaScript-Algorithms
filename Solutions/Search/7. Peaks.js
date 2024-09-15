55a599eaf6c664ec821a02991241c79f28c31a6b
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-09-15 19:47:35 +0900

    Problems: Peaks 재풀이

--- Solutions/Search/7. Peaks.js
+++ Solutions/Search/7. Peaks.js
@@ -32,30 +32,58 @@
 
 */
 
+
 function solution(arr) {
-  let answer = 0;
-  let n = arr.length;
-  let dx = [-1, 0, 1, 0];
-  let dy = [0, 1, 0, -1];
+  let count = 0;
+  const n = arr.length;
+  const dx = [-1, 0, 1, 0];
+  const dy = [0, 1, 0, -1];
 
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-      let flag = 1;
+      let isPeak = true;
 
       for (let k = 0; k < 4; k++) {
-        let nx = i + dx[k];
-        let ny = j + dy[k];
-        if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
-          flag = 0;
+        const nx = i + dx[k];
+        const ny = j + dy[k];
+
+        if (nx >= 0 && ny >= 0 && nx < n && ny < n && arr[nx][ny] >= arr[i][j]) {
+          isPeak = false;
           break;
         }
       }
-      if (flag) answer++;
+
+      if (isPeak) count++;
     }
   }
 
-  return answer;
+  return count;
 }
+
+// function solution(arr) {
+//   let answer = 0;
+//   let n = arr.length;
+//   let dx = [-1, 0, 1, 0];
+//   let dy = [0, 1, 0, -1];
+
+//   for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n; j++) {
+//       let flag = 1;
+
+//       for (let k = 0; k < 4; k++) {
+//         let nx = i + dx[k];
+//         let ny = j + dy[k];
+//         if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
+//           flag = 0;
+//           break;
+//         }
+//       }
+//       if (flag) answer++;
+//     }
+//   }
+
+//   return answer;
+// }
 
 let arr = [[5, 3, 7, 2, 3],
 [3, 7, 1, 6, 1],
