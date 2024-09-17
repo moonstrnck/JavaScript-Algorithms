d275d595b9f5fcd18a44278a2650d94086b33719
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-09-17 15:14:45 +0900

    Problems: SumOfDigits 문제 풀이

--- Solutions/BruteForce/1. SumOfDigits.js
+++ Solutions/BruteForce/1. SumOfDigits.js
@@ -1,7 +1,6 @@
 /*
 자릿수의 합
-N개의  자연수가  입력되면  각  자연수의  자릿수의  합을  구하고,  그  합이  최대인  자연수를  출력
-하는 프로그램을 작성하세요. 자릿수의 합이 같은 경우 원래 숫자가 큰 숫자를 답으로 합니다.
+N개의  자연수가  입력되면  각  자연수의  자릿수의  합을  구하고,  그  합이  최대인  자연수를  출력하는 프로그램을 작성하세요. 자릿수의 합이 같은 경우 원래 숫자가 큰 숫자를 답으로 합니다.
 만약 235 와 1234가 동시에 답이 될 수 있다면 1234를 답으로 출력해야 합니다.
 
 ▣ 입력설명
@@ -21,7 +20,26 @@ N개의  자연수가  입력되면  각  자연수의  자릿수의  합을  �
 */
 
 function solution(n, arr) {
+  let maxSum = 0;
+  let maxNum = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    const digit = arr[i] + '';
+    let sum = 0;
+    for (let j = 0; j < digit.length; j++) {
+      sum += parseInt(digit[j]);
+      if (maxSum < sum) {
+        maxSum = sum;
+        maxNum = arr[i];
+      }
+
+      if (maxSum === sum && maxNum < arr[i]) {
+        maxNum = arr[i];
+      }
+    }
+  }
+
+  return maxNum;
 }
 
 let arr = [128, 460, 603, 40, 521, 137, 123];
