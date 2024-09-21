8cd1e8759972405f73e415196d1790676680787a
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-09-21 23:45:35 +0900

    Problems: KthNumber 문제 풀이

--- Solutions/BruteForce/5. KthNumber.js
+++ Solutions/BruteForce/5. KthNumber.js
@@ -22,7 +22,18 @@ K값이 3이라면 K번째 큰 값은 22입니다.
 */
 
 function solution(n, k, card) {
-
+  let answer;
+  let tmp = new Set();
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let k = j + 1; k < n; k++) {
+        tmp.add(card[i] + card[j] + card[k]);
+      }
+    }
+  }
+  let a = Array.from(tmp).sort((a, b) => b - a);
+  answer = a[k - 1];
+  return answer;
 }
 
 let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
